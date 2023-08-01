8951a8e5deb739c415e0afe94737bd82c697febd
Rubén Capetillo <jose.capetillo@digitalfemsa.com>
2023-08-01 00:37:58 -0600

    chore(*): iteration 1: implementing authservice

---
+++ src/AuthService.ts
@@ -0,0 +1,55 @@
+interface Users {
+  id: number;
+  username: string;
+  password: string;
+}
+
+class AuthService {
+
+  private readonly registeredUsers: Users[] = [
+    {
+      id: new Date().getTime() * Math.trunc(Math.random() * 3),
+      username: 'johnwick',
+      password: '123456'
+    },
+    {
+      id: new Date().getTime() * Math.trunc(Math.random() * 3),
+      username: 'brucewayne',
+      password: 'gothamcity'
+    },
+    {
+      id: new Date().getTime() * Math.trunc(Math.random() * 3),
+      username: 'wadewinston',
+      password: 'sh00t&run'
+    },
+    {
+      id: new Date().getTime() * Math.trunc(Math.random() * 3),
+      username: 'mrrobot',
+      password: '3ll10t'
+    },
+  ]
+
+  register(username: string, password:string):Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      this.registeredUsers.find(user => {
+        if(user.username === username || user.password === password) {
+          reject(false)
+        } else {
+          resolve(true)
+        }
+      })
+    })
+  }
+
+  login(username: string, password: string): Promise<boolean> {
+    return new Promise ((resolve, reject) => {
+      this.registeredUsers.find(user => {
+        if(user.username === username && user.password === password) {
+          resolve(true)
+        } else {
+          reject(false)
+        }
+      })
+    })
+  }
+}
